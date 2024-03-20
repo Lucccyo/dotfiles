caae909686881d50c27d64c5835fc442f86be6e5
Lucccyo <cha.git@mailo.fr>
2024-03-20 10:25:51 +0100

    script with modules shading

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,10 +7,10 @@ alacritty_config="$HOME/.config/alacritty/alacritty.toml"
 catppuccin_frappe_modifs="$HOME/.config/nvim/lua/plugins/catppuccin.lua"
 
 set_waybar_colours() {
-  # $1 -> line of the target colour
+  # $1 -> string of colour "#00ffff"
   # $2 -> identifier
-  colour=$(sed -n "$1p" < "${colours_file}")
-  sed -ri "s|^@define-color $2.*|@define-color $2 ${colour};|g" "${waybar_style}"
+  # colour=$(sed -n "$1p" < "${colours_file}")
+  sed -ri "s|^@define-color $2.*|@define-color $2 $1;|g" "${waybar_style}"
 }
 
 set_alacritty_colours() {
@@ -27,6 +27,11 @@ set_catppuccin_frappe_colours() {
   sed -ri "s|^( *$2 = ).*|\1\"${colour}\",|g" "${catppuccin_frappe_modifs}"
 }
 
+get_colour() {
+  # $1 -> line of the target colour
+  echo "$(sed -n "$1p" < "${colours_file}")"
+}
+
 ## setting the wallpaper in sway config
 sed -ri "s|^output \* bg.*|output \* bg $wallpaper_path fill|g" "${sway_config}"
 
@@ -34,8 +39,20 @@ sed -ri "s|^output \* bg.*|output \* bg $wallpaper_path fill|g" "${sway_config}"
 python "$current_dir_path/"gen_color.py "${wallpaper_path}" 30 > ${colours_file}
 
 ## changing colours in waybar
-set_waybar_colours 3 "background"
-set_waybar_colours $(wc -l < ${colours_file}) "foreground"
+colour_l3=$(get_colour 3)
+set_waybar_colours "$colour_l3" "background"
+colour_light_bg=$(python "$current_dir_path/"illuminate.py "$colour_l3" 0.1)
+set_waybar_colours "$colour_light_bg" "unfocused"
+colour_lmax=$(get_colour $(wc -l < ${colours_file}))
+set_waybar_colours "$colour_lmax" "text"
+colour_l24=$(get_colour 24)
+set_waybar_colours "$colour_l24" "focused"
+colour_basex="$colour_light_bg"
+for i in {7..1}; do
+  colour_basex=$(python "$current_dir_path/"illuminate.py "$colour_basex" 0.03)
+  set_waybar_colours "$colour_basex" "base$i"
+done
+
 
 ## changing colours in alacritty
 set_alacritty_colours 3 "background"
