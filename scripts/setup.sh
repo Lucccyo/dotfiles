470f7714e2ca75d3bcd8199b6bdb64ea111c0211
Lucccyo <cha.git@mailo.fr>
2024-03-20 10:48:10 +0100

    style edit

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,22 +9,19 @@ catppuccin_frappe_modifs="$HOME/.config/nvim/lua/plugins/catppuccin.lua"
 set_waybar_colours() {
   # $1 -> string of colour "#00ffff"
   # $2 -> identifier
-  # colour=$(sed -n "$1p" < "${colours_file}")
   sed -ri "s|^@define-color $2.*|@define-color $2 $1;|g" "${waybar_style}"
 }
 
 set_alacritty_colours() {
-  # $1 -> line of the target colour
+  # $1 -> string of colour "#00ffff"
   # $2 -> identifier
-  colour=$(sed -n "$1p" < "${colours_file}")
-  sed -ri "s|^$2.*|$2 = \"${colour}\"|g" "${alacritty_config}"
+  sed -ri "s|^$2.*|$2 = \"$1\"|g" "${alacritty_config}"
 }
 
 set_catppuccin_frappe_colours() {
-  # $1 -> line of the target colour
+  # $1 -> string of colour "#00ffff"
   # $2 -> identifier
-  colour=$(sed -n "$1p" < "${colours_file}")
-  sed -ri "s|^( *$2 = ).*|\1\"${colour}\",|g" "${catppuccin_frappe_modifs}"
+  sed -ri "s|^( *$2 = ).*|\1\"$1\",|g" "${catppuccin_frappe_modifs}"
 }
 
 get_colour() {
@@ -36,31 +33,39 @@ get_colour() {
 sed -ri "s|^output \* bg.*|output \* bg $wallpaper_path fill|g" "${sway_config}"
 
 ## picking colours from image
-python "$current_dir_path/"gen_color.py "${wallpaper_path}" 30 > ${colours_file}
+python "$current_dir_path/"gen_colour.py "${wallpaper_path}" 30 > ${colours_file}
+
+## redundant colours
+colour_l3=$(get_colour 3) # background
+colour_l24=$(get_colour 24) # main colour
+colour_light_l3=$(python "$current_dir_path/"illuminate.py "$colour_l3" 0.1) # subbackground
+colour_lmax=$(get_colour $(wc -l < ${colours_file})) # text
 
 ## changing colours in waybar
-colour_l3=$(get_colour 3)
 set_waybar_colours "$colour_l3" "background"
-colour_light_bg=$(python "$current_dir_path/"illuminate.py "$colour_l3" 0.1)
-set_waybar_colours "$colour_light_bg" "unfocused"
-colour_lmax=$(get_colour $(wc -l < ${colours_file}))
+set_waybar_colours "$colour_light_l3" "unfocused"
 set_waybar_colours "$colour_lmax" "text"
-colour_l24=$(get_colour 24)
 set_waybar_colours "$colour_l24" "focused"
+## with background's modules shading
+# colour_basex="$colour_light_l3"
+# for i in {7..1}; do
+#   colour_basex=$(python "$current_dir_path/"illuminate.py "$colour_basex" 0.03)
+#   set_waybar_colours "$colour_basex" "base$i"
+# done
+## with flat colour
 for i in {7..1}; do
-  set_waybar_colours "$colour_light_bg" "base$i"
+  set_waybar_colours "$colour_light_l3" "base$i"
 done
 
-
 ## changing colours in alacritty
-set_alacritty_colours 3 "background"
-set_alacritty_colours $(wc -l < ${colours_file}) "foreground"
-set_alacritty_colours 27 "blue" # prompt colour
-set_alacritty_colours 24 "yellow" # git branch colour
+set_alacritty_colours "$colour_l3" "background"
+set_alacritty_colours "$colour_lmax" "foreground"
+set_alacritty_colours "$colour_l24" "blue" # prompt colour
+set_alacritty_colours "$colour_l24" "yellow" # git branch colour
 
-## changing colours in nvim through the catppuccin colortheme
-set_catppuccin_frappe_colours 3 "base"
-set_catppuccin_frappe_colours $(wc -l < ${colours_file}) "text"
-set_catppuccin_frappe_colours 24 "mantle" # bar
+## changing colours in nvim through the catppuccin colourtheme
+set_catppuccin_frappe_colours "$colour_l3" "base"
+set_catppuccin_frappe_colours "$colour_lmax" "text"
+set_catppuccin_frappe_colours "$colour_l24" "mantle" # bar
 
 sway reload
